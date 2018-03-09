6aaa2b1a81e1c53bb44965fcd304731dee3ac994
Christopher Bertels <bakkdoor@flasht.de>
2018-03-09 02:15:40 +0100

    Bump to elixir 1.6

--- mix.exs
+++ mix.exs
@@ -6,20 +6,25 @@ defmodule Bertex.Mixfile do
   """
 
   def project do
-    [ app: :bertex,
-      version: "1.4.1",
-      elixir: "~> 0.14.2 or ~> 0.15.0 or ~> 1.0.0 or ~> 1.2.0 or ~> 1.3.0 or ~> 1.4.0 or ~> 1.5.0",
+    [
+      app: :bertex,
+      version: "1.4.2",
+      elixir:
+        "~> 0.14.2 or ~> 0.15.0 or ~> 1.0.0 or ~> 1.2.0 or ~> 1.3.0 or ~> 1.4.0 or ~> 1.5.0 or ~> 1.6.0",
       name: "Bertex",
       description: @description,
       package: package(),
-      deps: [] ]
+      deps: []
+    ]
   end
 
   def application, do: []
 
   defp package do
-    [ contributors: ["Eduardo Gurgel Pinho", "Christopher Bertels"],
+    [
+      contributors: ["Eduardo Gurgel Pinho", "Christopher Bertels"],
       licenses: ["MIT"],
-      links: [ { "Github", "https://github.com/edgurgel/bertex" } ] ]
+      links: [{"Github", "https://github.com/edgurgel/bertex"}]
+    ]
   end
 end
